df51f1a48bf9c7562b75e9ab8dffa70bcf2a9446
kristofpaulusz <paulusz.kristof.csanad@diak.szamalk-szalezi.hu>
2022-09-26 14:33:13 +0200

    lehet neveket adni

--- Jatekter.js
+++ Jatekter.js
@@ -1,5 +1,7 @@
 import Negyzet from "../Negyzet.js";
 class Jatekter {
+  jatekos1 = "X";
+  jatekos2 = "O";
   #negyzetek = [];
   #kovetkezo = false;
   #szuloElem;
@@ -7,7 +9,19 @@ class Jatekter {
     this.#szuloElem = szuloElem;
 
     //Jatekter felépítése:
-    this.#szuloElem.append("<div class='jatekAllapot'>Az X jön</div>");
+    this.#szuloElem.append(`<div class="jatekosok">
+    <form id="myForm" onsubmit="return false">
+      <label for="name1">1.es játékos:</label>
+      <input type="text" name="name1">
+      <label for="name2">2.es játékos:</label>
+      <input type="text" name="name2">
+      <input id="button" type="submit" value="Submit">
+    </form></div>`);
+    this.jatekosok = szuloElem.children("div:last-child");
+
+    this.#szuloElem.append(
+      `<div class='jatekAllapot'>${this.jatekos1} jön</div>`
+    );
     this.jatekAllapotDiv = szuloElem.children("div:last-child");
 
     this.#szuloElem.append("<div class='tabla'></div>");
@@ -19,11 +33,16 @@ class Jatekter {
     for (let index = 0; index < 9; index++) {
       this.#negyzetek.push(new Negyzet(index, 0, $(this.jatekterDiv)));
     }
-
+    document.forms["myForm"].addEventListener("submit", () => {
+      this.jatekos1 = document.forms["myForm"]["name1"].value;
+      this.jatekos2 = document.forms["myForm"]["name2"].value;
+      this.tablaUpdate(this.#kovetkezo);
+    });
     $(window).on("lepes", () => {
       this.kor();
     });
   }
+  jatekosNevek() {}
   kor() {
     this.#kovetkezo = !this.#kovetkezo;
     let para = this.#kovetkezo;
@@ -33,8 +52,8 @@ class Jatekter {
   }
   tablaUpdate(allapot) {
     if (allapot) {
-      this.jatekAllapotDiv.html("Az O jön");
-    } else this.jatekAllapotDiv.html("Az X jön");
+      this.jatekAllapotDiv.html(`${this.jatekos2} jön`);
+    } else this.jatekAllapotDiv.html(`${this.jatekos1} jön`);
   }
   vaneNyeres() {
     let nyeres = false;
@@ -43,63 +62,56 @@ class Jatekter {
       this.#negyzetek[1].getJatekos() == this.#negyzetek[2].getJatekos() &&
       this.#negyzetek[0].getJatekos() != 0
     ) {
-      console.log("It's a win 0 1 2");
+      // console.log("It's a win 0 1 2");
       nyeres = true;
-    }
-    if (
+    } else if (
       this.#negyzetek[3].getJatekos() == this.#negyzetek[4].getJatekos() &&
       this.#negyzetek[4].getJatekos() == this.#negyzetek[5].getJatekos() &&
       this.#negyzetek[3].getJatekos() != 0
     ) {
-      console.log("It's a win 3 4 5");
+      // console.log("It's a win 3 4 5");
       nyeres = true;
-    }
-    if (
+    } else if (
       this.#negyzetek[6].getJatekos() == this.#negyzetek[7].getJatekos() &&
       this.#negyzetek[7].getJatekos() == this.#negyzetek[8].getJatekos() &&
       this.#negyzetek[6].getJatekos() != 0
     ) {
-      console.log("It's a win 6 7 8");
+      // console.log("It's a win 6 7 8");
       nyeres = true;
-    }
-    if (
+    } else if (
       this.#negyzetek[0].getJatekos() == this.#negyzetek[3].getJatekos() &&
       this.#negyzetek[3].getJatekos() == this.#negyzetek[6].getJatekos() &&
       this.#negyzetek[0].getJatekos() != 0
     ) {
-      console.log("It's a win 0 3 6");
+      // console.log("It's a win 0 3 6");
       nyeres = true;
-    }
-    if (
+    } else if (
       this.#negyzetek[1].getJatekos() == this.#negyzetek[4].getJatekos() &&
       this.#negyzetek[4].getJatekos() == this.#negyzetek[7].getJatekos() &&
       this.#negyzetek[1].getJatekos() != 0
     ) {
-      console.log("It's a win 1 4 7");
+      // console.log("It's a win 1 4 7");
       nyeres = true;
-    }
-    if (
+    } else if (
       this.#negyzetek[2].getJatekos() == this.#negyzetek[5].getJatekos() &&
       this.#negyzetek[5].getJatekos() == this.#negyzetek[8].getJatekos() &&
       this.#negyzetek[2].getJatekos() != 0
     ) {
-      console.log("It's a win 2 5 8");
+      // console.log("It's a win 2 5 8");
       nyeres = true;
-    }
-    if (
+    } else if (
       this.#negyzetek[0].getJatekos() == this.#negyzetek[4].getJatekos() &&
       this.#negyzetek[4].getJatekos() == this.#negyzetek[8].getJatekos() &&
       this.#negyzetek[0].getJatekos() != 0
     ) {
-      console.log("It's a win 0 4 8");
+      // console.log("It's a win 0 4 8");
       nyeres = true;
-    }
-    if (
+    } else if (
       this.#negyzetek[2].getJatekos() == this.#negyzetek[4].getJatekos() &&
       this.#negyzetek[4].getJatekos() == this.#negyzetek[6].getJatekos() &&
       this.#negyzetek[2].getJatekos() != 0
     ) {
-      console.log("It's a win 2 4 6");
+      // console.log("It's a win 2 4 6");
       nyeres = true;
     }
     if (nyeres) {
@@ -110,9 +122,9 @@ class Jatekter {
     $(window).off("lepes");
     let nyertes;
     if (this.#kovetkezo) {
-      nyertes = "X";
-    } else nyertes = "O";
-    this.tablaDiv.html("Az " + nyertes + " nyert");
+      nyertes = this.jatekos1;
+    } else nyertes = this.jatekos2;
+    this.tablaDiv.html(nyertes + " nyert");
     this.jatekAllapotDiv.html("Vége");
   }
 }
